319aa79a71af59751eb69231c011a32c1bd0c494
AIZAWA Hina <hina@fetus.jp>
2022-02-12 13:13:05 +0900

    Noto Serif JP

---
+++ dist/fonts/noto-serif-jp-subset/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -ue
+
+wget -O noto-serif-jp-bold.woff     https://github.com/ixkaito/NotoSerifJP-subset/raw/master/subset/NotoSerifCJKjp-Bold.woff
+wget -O noto-serif-jp-regular.woff  https://github.com/ixkaito/NotoSerifJP-subset/raw/master/subset/NotoSerifCJKjp-Regular.woff
